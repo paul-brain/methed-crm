193d9cc07f7d4da90d40ff3f4d8dd83d235807af
Paul Brain <paul_brain@mail.ru>
2023-05-23 10:22:35 +0300

    Modul 5 lesson 2: created main.js, added modificators

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-let productName = 'Samsung Galaxy A53';
-let productCount = 5;
-let productCategory = 'Mobile phones';
-let productPrice = 250;
-
-console.log('Имя товара:', productName);
-console.log('Общая сумма:', productPrice * productCount, '$');
+const modalHeader = document.querySelector('.modal__title');
+const modalCloseBtn = document.querySelector('.modal__btn-close');
+const goodId = document.querySelector('.modal__info');
+const form = document.querySelector('.form');
+const discontCheckbox = document.getElementsByName('discont');
+const discontText = document.getElementsByName('discont_text');
+const totalPrice = document.querySelector('.form__total-price');
