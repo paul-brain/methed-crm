65008c2a069c4d283a718e6bd25a2146145c6a4d
Paul Brain <paul_brain@mail.ru>
2023-06-27 16:52:59 +0300

    Modul 6-6 ready: ajax (glitch, fetch goods)

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-import {goods} from './goods.js';
+import {getGoods} from './modules/ajax.js';
 import {renderGoods} from './modules/render.js';
 import control from './modules/control.js';
 
@@ -9,11 +9,23 @@ const {
   imgControl,
 } = control;
 
-const init = () => {
+const init = async () => {
+  const goods = await getGoods();
   const tableBody = document.querySelector('.products__list tbody');
   const addBtn = document.querySelector('.products__add-btn');
   const modalOverlay = document.querySelector('.overlay');
   const form = document.querySelector('.form');
+
+  // Для демонтсрации ошибки при удалении
+  goods.unshift({
+    id: '1234567890',
+    title: 'Манго - для удаления',
+    description: 'Сочный, вкусный',
+    category: 'Фрукты',
+    price: 3000,
+    units: 'кг',
+    count: 30,
+  });
 
   renderGoods(tableBody, goods);
   deleteControl(tableBody, goods);
---
+++ js/modules/ajax.js
@@ -0,0 +1,78 @@
+const fetchRequest = async (url, {
+  method = 'GET',
+  callback,
+  callbackError,
+  body,
+  headers,
+}) => {
+  try {
+    const options = {
+      method,
+    };
+
+    if (body) options.body = JSON.stringify(body);
+    if (headers) options.headers = headers;
+
+    const response = await fetch(url, options);
+    const data = await response.json();
+
+    if (response.ok) {
+
+      if (callback) callback(null, data);
+
+      return data;
+    }
+
+    throw new Error(`Ошибка ${response.status}: ${data.message}`);
+  } catch (err) {
+    callbackError(err.message);
+  }
+};
+export const errorNotice = (message) => {
+  const modal = document.querySelector('.modal-wrong');
+  const modalText = document.querySelector('.modal-wrong__text');
+  const modalOverlay = document.querySelector('.overlay');
+
+  if (message.includes('Ошибка')) {
+    modalText.textContent = message;
+  } else {
+    modalText.textContent = 'Что-то пошло не так';
+  }
+
+  modalOverlay.classList.add('overlay--show');
+  modal.classList.add('modal-wrong--show');
+};
+
+export const getGoods = () => {
+  const url = 'https://knowledgeable-mammoth-parka.glitch.me/api/goods';
+  const options = {
+    method: 'GET',
+  };
+  const goods = fetchRequest(url, options);
+
+  return goods;
+};
+
+export const addGood = (product) => {
+  const url = 'https://knowledgeable-mammoth-parka.glitch.me/api/goods';
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+    body: product,
+    callbackError: errorNotice,
+  };
+  const good = fetchRequest(url, options);
+
+  return good;
+};
+
+export const deleteGood = (id) => {
+  const url = `https://knowledgeable-mammoth-parka.glitch.me/api/goods/${id}`;
+  const options = {
+    method: 'delete',
+    callbackError: errorNotice,
+  };
+  fetchRequest(url, options);
+};
